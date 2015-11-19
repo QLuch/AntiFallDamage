537f2672059aa6307b5602cb516e8757b1c25052
robozeri <robozerirobot@gmail.com>
2015-11-19 15:52:03 +0100

    Update main.php

--- AFD/main.php
+++ AFD/main.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace AFD;
+
+use PocketMine\plugin\PluginBase;
+use Pocketmine\event\entity\EntityDamageEvent
+use Pocketmine\event\Listener;
+class main extends PluginBase implements Listener {
+  
+  public void OnEnable{
+    
+    $this->log ( TextFormat::GREEN . "Anti Fall Damage - ENABLE" );
+    
+  }
+  public void OnDisable{
+    
+    $this->log ( TextFormat::GREEN . "Anti Fall Damage - DISABLE" );
+  }
+  public function onDamage(EntityDamageEvent $event){
+  if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
+    $event->setCancelled();
+  }
+}
+  public function log($msg) {
+		$this->getLogger ()->debug ( $msg );
+}
+  
